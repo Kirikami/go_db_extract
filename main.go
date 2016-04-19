b19c46a001928cb58f8847f94520bf879d646b04
Kirikami <naumenko.ruslan@outlook.com>
2016-04-19 21:29:00 +0700

    Added archiving

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 
 func main() {
 	configfile := flag.String("Configfile", "config.toml", "a string")
+	flag.Parse()
 	configs := MustNewConfig(configfile)
 	ch := make(chan string)
 	for _, config := range configs {
--- services/services.go
+++ services/services.go
@@ -1,16 +1,24 @@
 package services
 
 import (
+	"archive/tar"
+	"bytes"
 	"encoding/csv"
+	"fmt"
 	log "github.com/Sirupsen/logrus"
 	"github.com/kirikami/go_db_extract/config"
 	"github.com/kirikami/go_db_extract/database"
+	"io"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func generateCSV(tablename, filepath string, records []string) {
+var (
+	ErrCantReadFile = errors.New("Cant read file")
+)
+
+func generateCSV(tablename, filepath string, records []string) err {
 	file, err := os.Create(filepath + tablename + ".csv")
 	if err != nil {
 		log.Fatalf("Cannot create file: %s", err)
@@ -22,6 +30,10 @@ func generateCSV(tablename, filepath string, records []string) {
 		if err != nil {
 			log.Fatalf("Cannot write to file: %s", err)
 		}
+	}
+	err := writer.Write("There are %d records in databse")
+	if err != nil {
+		log.Fatalf("Cannot write to file: %s", err)
 	}
 	defer writer.Flush()
 }
@@ -56,6 +68,56 @@ func SalesTableDataProvider(db *sqlx.DB, c *Config) {
 
 }
 
-func archiveFile(filename string) {
+func archiveFile(source, target string) error {
+	filename := filepath.Base(source)
+	target = filepath.Join(target, fmt.Sprintf("%s.tar", filename))
+	tarfile, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer tarfile.Close()
 
+	tarball := tar.NewWriter(tarfile)
+	defer tarball.Close()
+
+	info, err := os.Stat(source)
+	if err != nil {
+		return nil
+	}
+
+	var baseDir string
+	if info.IsDir() {
+		baseDir = filepath.Base(source)
+	}
+
+	return filepath.Walk(source,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			header, err := tar.FileInfoHeader(info, info.Name())
+			if err != nil {
+				return err
+			}
+
+			if baseDir != "" {
+				header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, source))
+			}
+
+			if err := tarball.WriteHeader(header); err != nil {
+				return err
+			}
+
+			if info.IsDir() {
+				return nil
+			}
+
+			file, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+			_, err = io.Copy(tarball, file)
+			return err
+		})
 }
